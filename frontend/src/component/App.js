45af5e3f84a39c11092cd2124f310b660f7ba929
Christiaan Seerden <christiaanseerden@live.nl>
2020-10-01 17:18:17 +0200

    include frontend in repo

---
+++ frontend/src/component/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/App.component.css'
+
+const App = () => {
+    return (
+        <div className="App">
+            App.js
+        </div>
+    )
+}
+
+export default App;
