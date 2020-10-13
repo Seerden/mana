f91cfa7f328b921165ee9edc5de3ddd689dc799d
Christiaan Seerden <christiaanseerden@live.nl>
2020-10-13 16:44:16 +0200

    Implement local List term deletion

--- frontend/src/component/List.js
+++ frontend/src/component/List.js
@@ -1,4 +1,4 @@
-import React, { memo, useState, useEffect } from "react";
+import React, { memo, useCallback, useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import './css/List.css';
 import { useRouteProps } from '../hooks/routerHooks';
@@ -10,12 +10,45 @@ const List = memo((props) => {
     const [terms, setTerms] = useState(null);
     const { match } = useRouteProps();
 
+    function updateTerms () {
+        if(list) {
+            setTerms(list.content.map((term, idx) => {
+                let termProps = {
+                    handleTermDelete,
+                    key: `list-term-${idx}`,
+                    idx: idx,
+                    term
+                }
+            
+                return (
+                    <ListTerm {...termProps} />
+                )
+            }))
+        }
+        
+    }
+
     useEffect(() => {
         getListFromDB({  _id: match.params.id }).then(res => {
             setList(res);
-            setTerms(res.content.map((term, idx) => <ListTerm key={`list-term-${idx}`} idx={idx+1} term={term} />))
+            console.log(list)
         })
     }, [])
+
+    useEffect(() => {
+        if (list) {
+            updateTerms();
+        }
+    }, [list])
+
+    const handleTermDelete = (idx) => {
+        console.log('deleting term at index', idx);
+
+        const updatedList = {...list}
+        updatedList.content.splice(idx, 1);
+        setList(updatedList);
+
+    }
 
     return (
         <div className="List">
@@ -24,11 +57,7 @@ const List = memo((props) => {
             { list &&
                 <>
                     <h1 className="List__name">{list.name} ({list.from} to {list.to})</h1>
-                    <Link
-                        className="Link-button"
-                        to={{pathname: `${match.params.id}/review`}}
-                    >Review!</Link>
-
+                    <Link className="Link-button" to={{pathname: `${match.params.id}/review`}}>Review!</Link>
                     <ul>
                         {terms}
                     </ul>
--- frontend/src/component/ListTerm.js
+++ frontend/src/component/ListTerm.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { memo, useState } from "react";
 
-const ListTerm = ({ term, idx }) => {
+const ListTerm = memo(({ handleTermDelete, term, idx }) => {
     const [isEditing, setIsEditing] = useState(false);
-    const [confirmDelete, setConfirmDelete ] = useState(false);
+    const [confirmingDelete, setConfirmingDelete ] = useState(false);
     const [isHovering, setHovering] = useState(false)
 
     const termStyles = {
@@ -10,8 +10,18 @@ const ListTerm = ({ term, idx }) => {
         // gridTemplateRows: `3fr ${confirmDelete ? '' : ''}`
     }
     const termDeleteStyles = {
-        backgroundColor: confirmDelete ? '' : null,
-        border: confirmDelete ? '2px solid orangered' : null
+        backgroundColor: confirmingDelete ? '' : null,
+        border: confirmingDelete ? '2px solid orangered' : null
+    }
+
+    const handleConfirmClick = (e, action) => {
+        e.preventDefault();
+        setConfirmingDelete(false);
+        if (action.type === 'delete') {
+            handleTermDelete(idx);
+        }
+
+
     }
 
     return (
@@ -19,24 +29,24 @@ const ListTerm = ({ term, idx }) => {
             onMouseEnter={() => setHovering(true)}
             onMouseLeave={() => setHovering(false)}
             className="List__term" style={ {...termStyles, ...termDeleteStyles} }>
-            <div className="List__term-index">{idx}</div>
+            <div className="List__term-index">{idx+1}</div>
             <div className="List__term-from">{term.from}</div>
             <div className="List__term-to">{term.to}</div>
             
-            { !isEditing && confirmDelete &&
+            { !isEditing && confirmingDelete &&
                 <span className="List__term-remove-confirm">
                     <input 
-                        onClick={() => setConfirmDelete(false)}
+                        onClick={(e) => handleConfirmClick(e, {type: 'delete'})}
                         className="remove-confirm-button remove-remove" type="button" value="delete"/>
                     <input 
-                        onClick={() => setConfirmDelete(false)}
+                        onClick={e => handleConfirmClick(e, 'keep')}
                         className="remove-confirm-button remove-keep" type="button" value="keep"/>
                 </span>
 
             }
-            {!isEditing && isHovering && !confirmDelete &&
+            {!isEditing && isHovering && !confirmingDelete &&
                 <input 
-                    onClick={() => setConfirmDelete(true)}
+                    onClick={() => setConfirmingDelete(true)}
                     className="List__term-remove" 
                     type="button" 
                     value="x" />
@@ -45,6 +55,6 @@ const ListTerm = ({ term, idx }) => {
             
         </li>
     )
-}
+})
 
 export default ListTerm
