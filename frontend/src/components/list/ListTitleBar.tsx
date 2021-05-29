05abb182a3aa40d18b4e08f8e6fde3fede0f24d0
Christiaan Seerden <christiaanseerden@live.nl>
2021-05-29 15:05:01 +0200

    Style ListTitleBar

--- frontend/src/components/list/ListTitleBar.tsx
+++ frontend/src/components/list/ListTitleBar.tsx
@@ -2,6 +2,8 @@ import { List } from "graphql/codegen-output";
 import React from "react";
 import ListDeleteButton from './ListDeleteButton';
 
+import './ListTitleBar.scss';
+
 interface ListTitleBarProps {
     handleListTitleBlur(e: any): void,
     list: List,
@@ -9,19 +11,21 @@ interface ListTitleBarProps {
 }
 
 const ListTitleBar = ({ handleListTitleBlur, list, handleDelete }: ListTitleBarProps) => {
+    const base = "List__TitleBar"
+
     return (
-        <section className="List__titlebar">
-            <div>
-                <span
+        <section className={`${base}`}>
+            <header className={`${base}__header`}>
+                <h3
                     contentEditable
                     suppressContentEditableWarning
                     onBlur={handleListTitleBlur}
-                    className="List__section--header"
+                    className={`${base}__header--listname`}
                 >
                     {list.name}
-                </span>
-                <span> ({list.from} to {list.to})</span>
-            </div>
+                </h3>
+                <span className={`${base}__header--languages`}>({list.from} to {list.to})</span>
+            </header>
 
             <ListDeleteButton {...{ handleDelete }} />
         </section>
