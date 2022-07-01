15ab8737ab9b6b00e2a031b02deeea6fdeebd21c
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-01 15:54:31 +0200

    (refactor) split up nested ternary in ListTerms render logic

--- frontend/src/components/list/ListTerms/ListTerms.tsx
+++ frontend/src/components/list/ListTerms/ListTerms.tsx
@@ -2,40 +2,34 @@ import SaturationFilter from "components/SaturationFilter/SaturationFilter";
 import "./Terms.scss";
 
 const ListTerms = ({ filter, setFilter, termsToDisplay, list }) => {
-    const showingString = filter.saturation.level
-        ? "Showing filtered list"
-        : "Showing all terms";
+	const showingString = filter.saturation.level
+		? "Showing filtered list"
+		: "Showing all terms";
 
-    return (
-        <section className="Terms">
-            <ul className="List__terms">
-                <div>
-                    <header className="Terms__header">Terms</header>
+	return (
+		<section className="Terms">
+			<ul className="List__terms">
+				<div>
+					<header className="Terms__header">Terms</header>
 
-                    <div className="Terms__filterinfo">
-                        {list && list.sessions?.length > 0 && (
-                            <SaturationFilter {...{ filter, setFilter }} />
-                        )}
-                        <span className="Terms__filterinfo--filterstring">
-                            {showingString}
-                        </span>
-                    </div>
-                </div>
+					<div className="Terms__filterinfo">
+						{list && list.sessions?.length > 0 && (
+							<SaturationFilter {...{ filter, setFilter }} />
+						)}
+						<span className="Terms__filterinfo--filterstring">{showingString}</span>
+					</div>
+				</div>
 
-                {termsToDisplay ? (
-                    termsToDisplay.length > 0 ? (
-                        termsToDisplay
-                    ) : (
-                        <div className="Terms__display--filtered">
-                            All terms were filtered out
-                        </div>
-                    )
-                ) : (
-                    <div>Loading terms..</div>
-                )}
-            </ul>
-        </section>
-    );
+				{!termsToDisplay && <>Loading terms...</>}
+
+				{termsToDisplay?.length === 0 && (
+					<div className="Terms__display--filtered">All terms were filtered out</div>
+				)}
+
+				{termsToDisplay?.length && termsToDisplay}
+			</ul>
+		</section>
+	);
 };
 
 export default ListTerms;
