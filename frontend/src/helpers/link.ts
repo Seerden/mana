9fa6bb439a18fc7cd854d3ab82a4c1c17b6aaa75
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-01 11:34:12 +0200

    (refactor) simplify logic in isActive

--- frontend/src/helpers/link.ts
+++ frontend/src/helpers/link.ts
@@ -1,8 +1,5 @@
 import { Location } from "react-router-dom";
 
 export function isActive(to: string, location: Location) {
-    if (to === location.pathname) {
-        return true;
-    }
-    return false;
+	return to === location.pathname;
 }
