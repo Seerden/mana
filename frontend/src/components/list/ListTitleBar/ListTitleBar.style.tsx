e05d1112417d122edbe524fb6cb6fd3d7123af6d
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-01 15:00:49 +0200

    (theme) refactor ListTitleBar from scss to Styled components

---
+++ frontend/src/components/list/ListTitleBar/ListTitleBar.style.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import { tempSection } from "../../../helpers/theme/theme";
+
+const regularPadding = "0.5rem 1rem";
+
+// .List__TitleBar
+export const TitleBar = styled.section`
+	${tempSection};
+
+	margin-bottom: 0;
+	padding: 0;
+	border: none;
+
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+// &__header
+export const TitleBarHeader = styled.header`
+	display: flex;
+`;
+
+// &--listname
+export const ListName = styled.h3`
+	background-color: ${(p) => p.theme.colors.blue.main};
+	color: ${(p) => p.theme.colors.dark.black};
+	font-weight: 400;
+	margin-bottom: 0;
+	padding: ${regularPadding};
+	min-width: 5rem;
+	text-align: center;
+`;
+
+// &--languages
+export const ListLanguages = styled.span`
+	margin-bottom: 0;
+	background-color: ${(p) => p.theme.colors.dark.grey.darker};
+	color: ${(p) => p.theme.colors.light.tint};
+	padding: ${regularPadding};
+	border: 2px solid ${(p) => p.theme.colors.blue.main};
+`;
--- frontend/src/components/list/ListTitleBar/ListTitleBar.tsx
+++ frontend/src/components/list/ListTitleBar/ListTitleBar.tsx
@@ -1,35 +1,34 @@
 import { List } from "gql/codegen-output";
 import ListDeleteButton from "../ListDeleteButton/ListDeleteButton";
-import "./ListTitleBar.scss";
+import * as S from "./ListTitleBar.style";
 
 interface ListTitleBarProps {
-    handleListTitleBlur(e): void;
-    list: List;
-    handleDelete(): void;
+	handleListTitleBlur(e): void;
+	list: List;
+	handleDelete(): void;
 }
 
 const ListTitleBar = ({ handleListTitleBlur, list, handleDelete }: ListTitleBarProps) => {
-    const base = "List__TitleBar";
+	const base = "List__TitleBar";
 
-    return (
-        <section className={`${base}`}>
-            <header className={`${base}__header`}>
-                <h3
-                    contentEditable
-                    suppressContentEditableWarning
-                    onBlur={handleListTitleBlur}
-                    className={`${base}__header--listname`}
-                >
-                    {list.name}
-                </h3>
-                <span className={`${base}__header--languages`}>
-                    ({list.from} to {list.to})
-                </span>
-            </header>
+	return (
+		<S.TitleBar>
+			<S.TitleBarHeader>
+				<S.ListName
+					contentEditable
+					suppressContentEditableWarning
+					onBlur={handleListTitleBlur}
+				>
+					{list.name}
+				</S.ListName>
+				<S.ListLanguages>
+					({list.from} to {list.to})
+				</S.ListLanguages>
+			</S.TitleBarHeader>
 
-            <ListDeleteButton {...{ handleDelete }} />
-        </section>
-    );
+			<ListDeleteButton {...{ handleDelete }} />
+		</S.TitleBar>
+	);
 };
 
 export default ListTitleBar;
