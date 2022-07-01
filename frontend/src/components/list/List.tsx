431b1d362ad15aa6e2b20c269500399539923d3f
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-01 14:30:20 +0200

    (refactor) don't memo() the List component

--- frontend/src/components/list/List.tsx
+++ frontend/src/components/list/List.tsx
@@ -1,10 +1,9 @@
 import useList from "hooks/list.hooks/useList";
-import { memo } from "react";
 import ListReviewButtons from "./ListReviewButtons/ListReviewButtons";
 import ListTerms from "./ListTerms/ListTerms";
 import ListTitleBar from "./ListTitleBar/ListTitleBar";
 
-const List = memo((props) => {
+function List() {
 	const { list, termsToDisplay, filter, handleListTitleBlur, handleDelete, setFilter } =
 		useList();
 
@@ -21,6 +20,6 @@ const List = memo((props) => {
 			</div>
 		</div>
 	);
-});
+}
 
 export default List;
