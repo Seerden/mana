40f144dde4444b27a43829b656e483a5963e4323
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-01 15:40:47 +0200

    (todo) add todo suggestion on using URLSearchParams

--- frontend/src/components/list/ListReviewButtons/ListReviewButtons.tsx
+++ frontend/src/components/list/ListReviewButtons/ListReviewButtons.tsx
@@ -6,6 +6,7 @@ const ListReviewButtons = () => {
 	return (
 		<S.ReviewButtons>
 			<S.Header>Review</S.Header>
+			{/* TODO: use URLSearchParams instead of manually building the path like this */}
 			<S.ReviewLink to={`review?kind=${kind}`}>Review all terms</S.ReviewLink>
 		</S.ReviewButtons>
 	);
