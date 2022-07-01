f9bd8873ca444335f06229e56d96fd853ec71f34
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-01 14:59:49 +0200

    (theme) add tempSection snippet equivalent to shared-temp-section scss snippet

--- frontend/src/helpers/theme/theme.tsx
+++ frontend/src/helpers/theme/theme.tsx
@@ -1,3 +1,4 @@
+import { css } from "styled-components";
 import { colors } from "./colors";
 
 export const theme = {
@@ -5,3 +6,10 @@ export const theme = {
 } as const;
 
 export type MainTheme = typeof theme;
+
+export const tempSection = css`
+	border: 3px solid ${(p) => p.theme.colors.dark.grey.darker};
+	padding: 1rem;
+	width: 800px;
+	margin-top: 1rem;
+`;
