8fd91c0bb2da3d0dbb7110b845e0892bd6084ea9
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-04 17:41:16 +0200

    (refactor) use map instead of raw loop to create termInputs, use optional onSuccess callback in useMutateCreateList instead of handling success case in an effect

--- frontend/src/gql/hooks/list-mutate.ts
+++ frontend/src/gql/hooks/list-mutate.ts
@@ -1,14 +1,14 @@
 import {
-    ListUpdateActionInput,
-    ListUpdatePayloadInput,
-    MaybeList,
-    NewListFromClientInput,
-    SuccessOrError,
+	ListUpdateActionInput,
+	ListUpdatePayloadInput,
+	MaybeList,
+	NewListFromClientInput,
+	SuccessOrError,
 } from "gql/codegen-output";
 import {
-    createListMutation,
-    deleteListMutation,
-    updateListMutation,
+	createListMutation,
+	deleteListMutation,
+	updateListMutation,
 } from "gql/operations/list-operations";
 import request from "graphql-request";
 import { useMutation } from "react-query";
@@ -16,49 +16,49 @@ import { useMutation } from "react-query";
 const uri = process.env.GRAPHQL_URI;
 
 export function useMutateDeleteList(id: string) {
-    const { mutate, data, ...rest } = useMutation<SuccessOrError, any, void>(
-        "deleteList",
-        async () => {
-            const response = await request(uri, deleteListMutation, { id });
-            return response.deleteList;
-        },
-        { retry: false }
-    );
-    return { mutate, data, ...rest };
+	const { mutate, data, ...rest } = useMutation<SuccessOrError, any, void>(
+		"deleteList",
+		async () => {
+			const response = await request(uri, deleteListMutation, { id });
+			return response.deleteList;
+		},
+		{ retry: false }
+	);
+	return { mutate, data, ...rest };
 }
 
-export function useMutateCreateList() {
-    const { mutate, data, ...rest } = useMutation<MaybeList, any, NewListFromClientInput>(
-        "createList",
-        async (newList) => {
-            const response = await request(uri, createListMutation, {
-                newList,
-            });
-            return response;
-        },
-        { retry: false }
-    );
-    return { mutate, data, ...rest };
+export function useMutateCreateList({ onSuccess }: { onSuccess?: (args?: any) => any }) {
+	const { mutate, data, ...rest } = useMutation<MaybeList, any, NewListFromClientInput>(
+		"createList",
+		async (newList) => {
+			const response = await request(uri, createListMutation, {
+				newList,
+			});
+			return response;
+		},
+		{ retry: false, onSuccess: onSuccess?.() }
+	);
+	return { mutate, data, ...rest };
 }
 
 type UpdateListVariables = {
-    listId: string;
-    action: ListUpdateActionInput;
-    payload: ListUpdatePayloadInput;
+	listId: string;
+	action: ListUpdateActionInput;
+	payload: ListUpdatePayloadInput;
 };
 
 export function useMutateUpdateList() {
-    const { mutate, data, ...rest } = useMutation<MaybeList, any, UpdateListVariables>(
-        "updateList",
-        async ({ listId, action, payload }) => {
-            const response = await request(uri, updateListMutation, {
-                listId,
-                action,
-                payload,
-            });
-            return response;
-        },
-        { retry: false }
-    );
-    return { mutate, data, ...rest };
+	const { mutate, data, ...rest } = useMutation<MaybeList, any, UpdateListVariables>(
+		"updateList",
+		async ({ listId, action, payload }) => {
+			const response = await request(uri, updateListMutation, {
+				listId,
+				action,
+				payload,
+			});
+			return response;
+		},
+		{ retry: false }
+	);
+	return { mutate, data, ...rest };
 }
