96165b3db1a2e4f2852abac34cec36aa3074ce89
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-04 20:17:40 +0200

    (chore) autoformat SaturationFilter files for new indentation

--- frontend/src/components/SaturationFilter/SaturationFilter.tsx
+++ frontend/src/components/SaturationFilter/SaturationFilter.tsx
@@ -5,88 +5,82 @@ import "./SaturationFilter.scss";
 import { useSaturationFilter } from "./useSaturationFilter";
 
 interface SaturationFilterProps {
-    filter: FilterInterface;
-    setFilter: React.Dispatch<React.SetStateAction<FilterInterface>>;
+	filter: FilterInterface;
+	setFilter: React.Dispatch<React.SetStateAction<FilterInterface>>;
 }
 
 const SaturationFilter = memo(({ setFilter }: SaturationFilterProps) => {
-    const {
-        filterDisplayState,
-        setFilterDisplayState,
-        saturationFilter,
-        setSaturationFilter,
-        icons,
-        directionButtons,
-    } = useSaturationFilter(setFilter);
+	const {
+		filterDisplayState,
+		setFilterDisplayState,
+		saturationFilter,
+		setSaturationFilter,
+		icons,
+		directionButtons,
+	} = useSaturationFilter(setFilter);
 
-    const labelButtonStyles: React.CSSProperties = {
-        boxShadow: `0 8px 0 -7px ${
-            saturationFilter
-                ? colorBySaturation(saturationFilter.level) || "#333"
-                : "#333"
-        }, 0 0 1rem black`,
-        border: `2px solid ${
-            saturationFilter
-                ? colorBySaturation(saturationFilter.level) || "#333"
-                : "#333"
-        }`,
-    };
+	const labelButtonStyles: React.CSSProperties = {
+		boxShadow: `0 8px 0 -7px ${
+			saturationFilter ? colorBySaturation(saturationFilter.level) || "#333" : "#333"
+		}, 0 0 1rem black`,
+		border: `2px solid ${
+			saturationFilter ? colorBySaturation(saturationFilter.level) || "#333" : "#333"
+		}`,
+	};
 
-    const iconsDivStyles: React.CSSProperties = {
-        border: `2px solid ${
-            saturationFilter ? colorBySaturation(saturationFilter.level) : "#333"
-        }`,
-    };
+	const iconsDivStyles: React.CSSProperties = {
+		border: `2px solid ${
+			saturationFilter ? colorBySaturation(saturationFilter.level) : "#333"
+		}`,
+	};
 
-    const filterDivStyles: React.CSSProperties = {
-        border: `2px solid ${
-            saturationFilter ? colorBySaturation(saturationFilter.level) : "#333"
-        }`,
-    };
+	const filterDivStyles: React.CSSProperties = {
+		border: `2px solid ${
+			saturationFilter ? colorBySaturation(saturationFilter.level) : "#333"
+		}`,
+	};
 
-    return (
-        <>
-            <div className="SaturationFilter">
-                {filterDisplayState === "initial" && (
-                    <>
-                        <button
-                            onClick={() => setFilterDisplayState("level")}
-                            className="SaturationFilter__label"
-                            style={labelButtonStyles}
-                        >
-                            Filter by saturation level
-                            <input
-                                type="button"
-                                value="Reset"
-                                className="SaturationFilter__reset"
-                                onClick={(e) => {
-                                    e.stopPropagation();
-                                    setSaturationFilter({
-                                        level: undefined,
-                                        direction: "any",
-                                    });
-                                }}
-                            />
-                        </button>
-                    </>
-                )}
+	return (
+		<>
+			<div className="SaturationFilter">
+				{filterDisplayState === "initial" && (
+					<>
+						<button
+							onClick={() => setFilterDisplayState("level")}
+							className="SaturationFilter__label"
+							style={labelButtonStyles}
+						>
+							Filter by saturation level
+							<input
+								type="button"
+								value="Reset"
+								className="SaturationFilter__reset"
+								onClick={(e) => {
+									e.stopPropagation();
+									setSaturationFilter({
+										level: undefined,
+										direction: "any",
+									});
+								}}
+							/>
+						</button>
+					</>
+				)}
 
-                {filterDisplayState === "level" && (
-                    <div className="SaturationFilter__icons" style={iconsDivStyles}>
-                        {icons}
-                    </div>
-                )}
+				{filterDisplayState === "level" && (
+					<div className="SaturationFilter__icons" style={iconsDivStyles}>
+						{icons}
+					</div>
+				)}
 
-                {filterDisplayState === "direction" && (
-                    <div className="SaturationFilter__filter" style={filterDivStyles}>
-                        <div className="SaturationFilter__direction">
-                            {directionButtons}
-                        </div>
-                    </div>
-                )}
-            </div>
-        </>
-    );
+				{filterDisplayState === "direction" && (
+					<div className="SaturationFilter__filter" style={filterDivStyles}>
+						<div className="SaturationFilter__direction">{directionButtons}</div>
+					</div>
+				)}
+			</div>
+		</>
+	);
 });
 
 export default SaturationFilter;
--- frontend/src/components/SaturationFilter/useSaturationFilter.tsx
+++ frontend/src/components/SaturationFilter/useSaturationFilter.tsx
@@ -5,81 +5,77 @@ import { FilterInterface } from "types/list.types";
 import SaturationIcon from "./SaturationIcon";
 
 export function useSaturationFilter(setFilter) {
-    const [saturationFilter, setSaturationFilter] = useState<
-        FilterInterface["saturation"]
-    >({
-        level: undefined,
-        direction: "any",
-    });
-    const [filterDisplayState, setFilterDisplayState] = useState("initial");
+	const [saturationFilter, setSaturationFilter] = useState<
+		FilterInterface["saturation"]
+	>({
+		level: undefined,
+		direction: "any",
+	});
+	const [filterDisplayState, setFilterDisplayState] = useState("initial");
 
-    useEffect(() => {
-        setFilter((cur) => ({ ...cur, saturation: saturationFilter }));
-    }, [saturationFilter, setFilter]);
+	useEffect(() => {
+		setFilter((cur) => ({ ...cur, saturation: saturationFilter }));
+	}, [saturationFilter, setFilter]);
 
-    function handleIconClick(level) {
-        setSaturationFilter((cur) => ({ ...cur, level }));
-        setFilterDisplayState("direction");
-    }
+	function handleIconClick(level) {
+		setSaturationFilter((cur) => ({ ...cur, level }));
+		setFilterDisplayState("direction");
+	}
 
-    function handleDirectionIconClick(direction) {
-        setSaturationFilter((cur) => ({ ...cur, direction }));
-        setFilterDisplayState("initial");
-    }
+	function handleDirectionIconClick(direction) {
+		setSaturationFilter((cur) => ({ ...cur, direction }));
+		setFilterDisplayState("initial");
+	}
 
-    function makeIcons() {
-        const icons = Object.keys(colorMap).map((level) => {
-            const levelNumber = Number(level);
-            return (
-                <React.Fragment key={`saturation-wrapper-${level}`}>
-                    <div
-                        onClick={() => {
-                            handleIconClick(level);
-                        }}
-                        className="SaturationFilter__icon--wrapper"
-                    >
-                        <SaturationIcon
-                            classes="SaturationFilter__icon"
-                            key={`saturation-filter-level-${level}`}
-                            saturation={levelNumber}
-                        />
-                    </div>
-                </React.Fragment>
-            );
-        });
-        return icons;
-    }
+	function makeIcons() {
+		const icons = Object.keys(colorMap).map((level) => {
+			const levelNumber = Number(level);
+			return (
+				<React.Fragment key={`saturation-wrapper-${level}`}>
+					<div
+						onClick={() => {
+							handleIconClick(level);
+						}}
+						className="SaturationFilter__icon--wrapper"
+					>
+						<SaturationIcon
+							classes="SaturationFilter__icon"
+							key={`saturation-filter-level-${level}`}
+							saturation={levelNumber}
+						/>
+					</div>
+				</React.Fragment>
+			);
+		});
+		return icons;
+	}
 
-    const icons = makeIcons();
+	const icons = makeIcons();
 
-    const directionButtons = ["forwards", "backwards", "any"].map((direction) => {
-        return (
-            <button
-                className="SaturationFilter__direction--button"
-                key={`saturation-filter-${direction}`}
-                value={direction}
-                onClick={() => handleDirectionIconClick(direction)}
-            >
-                <span>{direction}</span>
-                {direction !== "any" && (
-                    <span>
-                        {direction === "forwards" ? (
-                            <BiArrowToRight />
-                        ) : (
-                            <BiArrowToLeft />
-                        )}
-                    </span>
-                )}
-            </button>
-        );
-    });
+	const directionButtons = ["forwards", "backwards", "any"].map((direction) => {
+		return (
+			<button
+				className="SaturationFilter__direction--button"
+				key={`saturation-filter-${direction}`}
+				value={direction}
+				onClick={() => handleDirectionIconClick(direction)}
+			>
+				<span>{direction}</span>
+				{direction !== "any" && (
+					<span>
+						{direction === "forwards" ? <BiArrowToRight /> : <BiArrowToLeft />}
+					</span>
+				)}
+			</button>
+		);
+	});
 
-    return {
-        filterDisplayState,
-        setFilterDisplayState,
-        saturationFilter,
-        setSaturationFilter,
-        icons,
-        directionButtons,
-    } as const;
+	return {
+		filterDisplayState,
+		setFilterDisplayState,
+		saturationFilter,
+		setSaturationFilter,
+		icons,
+		directionButtons,
+	} as const;
 }
