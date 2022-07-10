2d773d844421b8d265ee37b2d3aca063810f654d
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-10 18:50:52 +0200

    (chore) rename and move authAtoms->auth to top-level src/state folder

--- frontend/src/hooks/useLogin.ts
+++ frontend/src/hooks/useLogin.ts
@@ -1,29 +1,29 @@
 import {
-    putUserInLocalStorage,
-    removeUserFromLocalStorage,
+	putUserInLocalStorage,
+	removeUserFromLocalStorage,
 } from "helpers/localStorage-helpers";
 import { useCallback, useMemo } from "react";
 import { useRecoilState } from "recoil";
-import { currentUserState } from "state/atoms/authAtoms";
+import { currentUserState } from "state/auth";
 
 export function useLogin() {
-    const [currentUser, setCurrentUser] = useRecoilState(currentUserState);
-    const isLoggedIn = useMemo(() => {
-        return currentUser ? true : false;
-    }, [currentUser]);
+	const [currentUser, setCurrentUser] = useRecoilState(currentUserState);
+	const isLoggedIn = useMemo(() => {
+		return currentUser ? true : false;
+	}, [currentUser]);
 
-    const login = useCallback(
-        (username) => {
-            putUserInLocalStorage(username);
-            setCurrentUser(username);
-        },
-        [currentUser, setCurrentUser]
-    );
+	const login = useCallback(
+		(username) => {
+			putUserInLocalStorage(username);
+			setCurrentUser(username);
+		},
+		[currentUser, setCurrentUser]
+	);
 
-    const logout = useCallback(() => {
-        setCurrentUser(null);
-        removeUserFromLocalStorage();
-    }, [currentUser, setCurrentUser]);
+	const logout = useCallback(() => {
+		setCurrentUser(null);
+		removeUserFromLocalStorage();
+	}, [currentUser, setCurrentUser]);
 
-    return { login, logout, currentUser, isLoggedIn };
+	return { login, logout, currentUser, isLoggedIn };
 }
