f68447667591d01a369304c9ff66f51b2ca3412c
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-10 19:21:51 +0200

    (refactor) create HistoryElement sub-subcomponent to replace makeHistoryElement helper

--- frontend/src/components/list/sub/TermHistory.tsx
+++ frontend/src/components/list/sub/TermHistory.tsx
@@ -1,30 +1,78 @@
-import { memo, useState } from "react";
-import { makeHistoryElement } from "../helpers/makeHistoryElement";
+import dayjs from "dayjs";
+import { useState } from "react";
+import { BiArrowToLeft, BiArrowToRight } from "react-icons/bi";
+import { v4 as uuidv4 } from "uuid";
+import { colors } from "../../../helpers/theme/colors";
+import { timeSince } from "../../../helpers/time";
+import PassfailIcon from "../../_shared/PassfailIcon";
 import * as S from "./TermHistory.style";
 
-const TermHistory = memo(({ history }: { history: any[] }) => {
+const TermHistory = ({ history }: { history: any[] }) => {
 	const [expand, setExpand] = useState(false);
 
-	const histEl = makeHistoryElement(history).reverse();
+	const historyElements = [...history]
+		.reverse()
+		.map((element) => <HistoryElement historyEntry={element} key={uuidv4()} />);
 
 	return (
 		<>
 			<S.History>
 				<S.Header>
 					<S.Description>
-						You've reviewed this term {histEl.length} time
-						{histEl.length === 1 ? "" : "s"}
+						You've reviewed this term {historyElements.length} time
+						{historyElements.length === 1 ? "" : "s"}
 					</S.Description>
-					{histEl.length > 1 && (
+					{historyElements.length > 1 && (
 						<S.ExpandButton onClick={() => setExpand(!expand)}>
 							{!expand ? "Showing one" : "Showing all"}
 						</S.ExpandButton>
 					)}
 				</S.Header>
-				<S.HistoryContent>{expand ? histEl : histEl[0]}</S.HistoryContent>
+				{historyElements.length > 0 && (
+					<S.HistoryContent>
+						{expand ? historyElements : historyElements[0]}
+					</S.HistoryContent>
+				)}
 			</S.History>
 		</>
 	);
-});
+};
 
 export default TermHistory;
+
+function HistoryElement({ historyEntry }) {
+	return (
+		<S.HistorySession>
+			<S.HistorySessionBlock>
+				<S.Direction>
+					{historyEntry.direction === "forwards" ? (
+						<BiArrowToRight
+							title="Reviewed front to back"
+							fill={colors.blue.main}
+							size={18}
+						/>
+					) : (
+						<BiArrowToLeft
+							title="Reviewed back to front"
+							// TODO: add to green theme value
+							fill="limegreen"
+							size={18}
+						/>
+					)}
+				</S.Direction>
+
+				<span title={dayjs(historyEntry.date).format("MMMM DD, YYYY (HH:mm)")}>
+					{timeSince(historyEntry.date)}
+				</span>
+			</S.HistorySessionBlock>
+
+			<S.HistorySessionBlock>
+				<div key={uuidv4()}>
+					{historyEntry.content.map((passfail, index) => (
+						<PassfailIcon key={`passfailicon-${index}`} {...{ passfail, index }} />
+					))}
+				</div>
+			</S.HistorySessionBlock>
+		</S.HistorySession>
+	);
+}
