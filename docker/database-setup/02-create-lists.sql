241b57d6cd675b45bf627c0ecde2eb11b1f96951
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-13 01:37:16 +0200

    (database) change lists table to use user_id instead of username

--- docker/database-setup/02-create-lists.sql
+++ docker/database-setup/02-create-lists.sql
@@ -1,6 +1,11 @@
 create table lists (
    list_id serial primary key not null,
-   username text  not null,
+   user_id serial not null,
+
+   constraint fk_list_user_id
+      foreign key (user_id)
+      references users(user_id),
+      
    name text not null,
    from_language text not null,
    to_language text not null,
