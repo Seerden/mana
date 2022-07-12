bafa39636349ae05eb6066248fde65a579b85e00
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-13 01:44:58 +0200

    (chore) run autoformatting on a file

--- backend/graphql/middleware/isAuthenticated.ts
+++ backend/graphql/middleware/isAuthenticated.ts
@@ -2,22 +2,22 @@ import { ExpressContext } from "apollo-server-express";
 import { createMethodDecorator, MiddlewareFn } from "type-graphql";
 
 export const isAuth: MiddlewareFn<ExpressContext> = ({ args, context }, next) => {
-    const loggedInUser = context.req.session.userId;
-    const userMakingRequest = args.username;
+   const loggedInUser = context.req.session.userId;
+   const userMakingRequest = args.username;
 
-    if (
-        !loggedInUser ||
-        !userMakingRequest ||
-        (loggedInUser && loggedInUser !== userMakingRequest)
-    ) {
-        throw new Error("Not authenticated");
-    } else {
-        return next();
-    }
+   if (
+      !loggedInUser ||
+      !userMakingRequest ||
+      (loggedInUser && loggedInUser !== userMakingRequest)
+   ) {
+      throw new Error("Not authenticated");
+   } else {
+      return next();
+   }
 };
 
 export function authenticated() {
-    return createMethodDecorator<ExpressContext>((context, next) => {
-        return isAuth(context, next);
-    });
+   return createMethodDecorator<ExpressContext>((context, next) => {
+      return isAuth(context, next);
+   });
 }
