b2b4432982dea6681daa3cbd48c085529390f3fa
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-12 17:43:14 +0200

    (refactor:UserResolver) remove unnecessary typedefs, and descriptions, and format for indentation

--- backend/graphql/resolvers/UserResolver.ts
+++ backend/graphql/resolvers/UserResolver.ts
@@ -3,45 +3,46 @@
 import { ExpressContext } from "apollo-server-express";
 import { Arg, Ctx, Field, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import { User } from "../types/User";
+import { createUser } from "./user/create-user";
 import { login } from "./user/login";
 import { queryAllUsers } from "./user/query-all-users";
 import { queryMe } from "./user/query-me";
 
 @ObjectType()
 class MaybeUser {
-    @Field(() => String, { nullable: true })
-    error?: String;
+   @Field({ nullable: true })
+   error?: string;
 
-    @Field(() => User, { nullable: true })
-    user?: User;
+   @Field(() => User, { nullable: true })
+   user?: User;
 }
 
 @Resolver()
 export class UserResolver {
-    @Query(() => [User], { name: "users" })
-    async users() {
-        return await queryAllUsers();
-    }
+   @Query(() => [User], { name: "users" })
+   async users() {
+      return await queryAllUsers();
+   }
 
-    @Query(() => MaybeUser, { description: "Returns currently logged in user. " })
-    async me(@Ctx() { req }: ExpressContext) {
-        return await queryMe(res);
-    }
+   @Query(() => MaybeUser)
+   async me(@Ctx() { req }: ExpressContext) {
+      return await queryMe(req);
+   }
 
-    @Mutation(() => MaybeUser)
-    async createUser(
-        @Arg("username") username: string,
-        @Arg("password") password: string
-    ): Promise<MaybeUser> {
-        return await createUser(username, password);
-    }
+   @Mutation(() => MaybeUser)
+   async createUser(
+      @Arg("username") username: string,
+      @Arg("password") password: string
+   ): Promise<MaybeUser> {
+      return await createUser(username, password);
+   }
 
-    @Mutation(() => MaybeUser, { description: "Login mutation" })
-    async login(
-        @Arg("username", (type) => String) username: string,
-        @Arg("password", (type) => String) password: string,
-        @Ctx() { req, res }: ExpressContext
-    ): Promise<MaybeUser> {
-        return await login(username, password, { req, res });
-    }
+   @Mutation(() => MaybeUser)
+   async login(
+      @Arg("username") username: string,
+      @Arg("password") password: string,
+      @Ctx() { req, res }: ExpressContext
+   ): Promise<MaybeUser> {
+      return await login(username, password, { req, res });
+   }
 }
