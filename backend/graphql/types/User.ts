68b35aac63af7da07143182f112c401595a05a84
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-12 17:41:52 +0200

    (typegraphql) update User type definition

--- backend/graphql/types/User.ts
+++ backend/graphql/types/User.ts
@@ -1,20 +1,12 @@
 import { Field, ObjectType } from "type-graphql";
-import { Ref } from "../../custom_types";
-import { List } from "./List";
 
 @ObjectType()
 export class User {
-    @Field(() => String)
-    _id: ObjectId;
+    @Field()
+    user_id: number;
 
-    @Field(() => String)
+    @Field()
     username: string;
-
-    @Field(() => [List], { nullable: true })
-    lists: Ref<List>[]; // mongoose ObjectId[]
-
-    @Field(() => String)
-    currentSession?: ObjectId; // mongoose ObjectId
 
     @Field()
     password: string;
