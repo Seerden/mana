cd8c74d4c1eaa5548cca8e17ffe4b311dba1156f
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-13 03:01:25 +0200

    (typegraphql) empty out ReviewSessionResolver pending refactor

--- backend/graphql/resolvers/ReviewSessionResolver.ts
+++ backend/graphql/resolvers/ReviewSessionResolver.ts
@@ -1,33 +1,5 @@
-// @ts-nocheck
+import { Resolver } from "type-graphql";
+import { ReviewSession } from "../types/ReviewSession";
 
-import { Arg, Field, Mutation, ObjectType, Query, Resolver } from "type-graphql";
-import { TermUpdateObject } from "../types/input_types/term";
-import { ReviewSession, ReviewSessionBase } from "../types/ReviewSession";
-import { createReviewSession } from "./review-session/create-review-session";
-import { queryReviewSessionsByUser } from "./review-session/query-by-user";
-
-@ObjectType()
-class MaybeReviewSession {
-    @Field(() => ReviewSession, { nullable: true })
-    savedReviewSession?: ReviewSession;
-
-    @Field({ nullable: true })
-    error?: string;
-}
-
-@Resolver((of) => ReviewSession)
-export class ReviewSessionResolver {
-    @Mutation(() => MaybeReviewSession)
-    async createReviewSession(
-        @Arg("newReviewSession") newReviewSession: ReviewSessionBase,
-        @Arg("termUpdateArray", (type) => [TermUpdateObject])
-        termUpdateArray: [TermUpdateObject]
-    ): Promise<MaybeReviewSession> {
-        return await createReviewSession(newReviewSession, termUpdateArray);
-    }
-
-    @Query(() => [ReviewSession])
-    async reviewSessionsByUser(@Arg("owner") owner: string) {
-        return await queryReviewSessionsByUser(owner);
-    }
-}
+@Resolver(() => ReviewSession)
+export class ReviewSessionResolver {}
