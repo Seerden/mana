d4ab3899ba4c5b7d16c2105475666aa8cb7603ca
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-14 01:36:20 +0200

    (typegraphql) add created_at field to User typedef, create UserInput type

--- backend/graphql/types/User.ts
+++ backend/graphql/types/User.ts
@@ -1,13 +1,25 @@
-import { Field, ObjectType } from "type-graphql";
+import { Field, InputType, Int, ObjectType } from "type-graphql";
 
 @ObjectType()
 export class User {
-    @Field()
-    user_id: number;
+   @Field()
+   username: string;
 
-    @Field()
-    username: string;
+   @Field()
+   password: string;
 
-    @Field()
-    password: string;
+   @Field()
+   user_id: number;
+
+   @Field(() => Int)
+   created_at: number;
+}
+
+@InputType("UserInput")
+export class UserInput implements Partial<User> {
+   @Field({ nullable: true })
+   username?: string;
+
+   @Field({ nullable: true })
+   password?: string;
 }
