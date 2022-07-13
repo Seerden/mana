88f31040ab23ea26ef8f2d4bded1b4469a2e6f02
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-14 01:27:01 +0200

    (refactor) extract destroySession function to a helper file

---
+++ backend/lib/destroy-session.ts
@@ -0,0 +1,22 @@
+import { ExpressContext } from "apollo-server-express";
+
+// TODO: probably best to extract this to an environment variable, or at least
+// a variable.
+const sessionString = "mana-session";
+
+export async function destroySession({ req, res }: ExpressContext) {
+   try {
+      if (!req.session?.userId) {
+         return { message: "Already logged out." };
+      }
+
+      res.clearCookie(sessionString);
+      req.session.destroy(null);
+
+      if (!req.session?.userId) {
+         return { message: "Logged out" };
+      }
+   } catch (error) {
+      throw new Error(error);
+   }
+}
