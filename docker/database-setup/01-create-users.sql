0de8e94f59ce11428f8eae92f98107aa17ad35d9
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-14 01:25:25 +0200

    (database) add created_at field to users table

--- docker/database-setup/01-create-users.sql
+++ docker/database-setup/01-create-users.sql
@@ -1,5 +1,6 @@
 create table users (
    user_id  serial   not null    primary key,
    username text     unique   not null,
-   password text              not null
+   password text              not null,
+   created_at timestamp default now()
 );
