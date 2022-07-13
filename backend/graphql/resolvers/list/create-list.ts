66feebdc030da8d403d078ce791510204aa51f39
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-13 13:08:23 +0200

    (fix) import renamed NewListFromClient->NewList

--- backend/graphql/resolvers/list/create-list.ts
+++ backend/graphql/resolvers/list/create-list.ts
@@ -1,10 +1,10 @@
 import { Error } from "postgres";
 import { sql } from "../../../db/init";
-import { NewListFromClient } from "../../types/input_types/list";
+import { NewList } from "../../types/input_types/list";
 import { List } from "../../types/List";
 import { Term } from "../../types/Term";
 
-export async function createList(newList: NewListFromClient) {
+export async function createList(newList: NewList) {
    const { user_id, from_language, name, to_language, terms } = newList;
 
    // Note that we don't really have to do this, since postgres would just
