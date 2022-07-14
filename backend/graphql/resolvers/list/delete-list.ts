684b58474bfabcc3f0f2d1089d9a24cc862197cc
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-14 02:10:41 +0200

    (type) update typing of deleteListsById to return delete items

--- backend/graphql/resolvers/list/delete-list.ts
+++ backend/graphql/resolvers/list/delete-list.ts
@@ -1,10 +1,18 @@
 import { sql } from "../../../db/init";
+import { List, ListAndTerms } from "../../types/List";
+import { Term } from "../../types/Term";
 
 // TODO: trycatch, typing
-export async function deleteListsById(listIds: number[]) {
+export async function deleteListsById(listIds: number[]): Promise<ListAndTerms> {
    return await sql.begin(async (sql) => {
-      const [list] = await sql`delete from lists where list_id in ${sql(listIds)}`;
-      const terms = await sql`delete from terms where list_id in ${sql(listIds)}`;
+      // TODO: currently, we don't have an `on delete cascade` definition in
+      // `lists`, so terms have to be deleted _first_, and then lists.
+      const terms = await sql<[Term?]>`delete from terms where list_id in ${sql(
+         listIds
+      )} returning *`;
+      const [list] = await sql<[List?]>`delete from lists where list_id in ${sql(
+         listIds
+      )} returning *`;
 
       return { list, terms };
    });
