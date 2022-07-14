26c8dc1ddbc5b7401dd76d4dc23cb7e26e4776cc
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-15 01:58:26 +0200

    (typegraphql) start implementing ReviewSessionResolver functions by adding createReviewSession mutation

--- backend/graphql/resolvers/ReviewSessionResolver.ts
+++ backend/graphql/resolvers/ReviewSessionResolver.ts
@@ -1,5 +1,30 @@
-import { Resolver } from "type-graphql";
-import { ReviewSession } from "../types/ReviewSession";
+import { Arg, Authorized, Int, Mutation, Query, Resolver } from "type-graphql";
+import { ReviewSession, ReviewSessionInput } from "../types/ReviewSession";
+import { ReviewSessionEntryInput } from "../types/ReviewSessionEntry";
+import { createReviewSession } from "./review-session/create-review-session";
 
 @Resolver(() => ReviewSession)
-export class ReviewSessionResolver {}
+export class ReviewSessionResolver {
+   @Query(() => [ReviewSession], { nullable: "items" })
+   @Authorized()
+   async sessionsById(
+      @Arg("user_id") user_id: number,
+      @Arg("sessionIds", () => [Int]) sessionIds: number[]
+   ) {
+      // fetch review sessions by id
+   }
+
+   @Query(() => [ReviewSession], { nullable: "items" })
+   @Authorized()
+   async sessionsByUser(@Arg("user_id") user_id: number) {
+      // fetch review sessions by user_id
+   }
+
+   @Mutation(() => ReviewSession)
+   async createSession(
+      @Arg("session") session: ReviewSessionInput,
+      @Arg("entries", () => [ReviewSessionEntryInput]) entries: ReviewSessionEntryInput[]
+   ) {
+      return createReviewSession(session, entries);
+   }
+}
