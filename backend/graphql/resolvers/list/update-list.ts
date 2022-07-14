dddc44552ea225a2c5751fd3e73e12416b04aabc
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-14 02:16:26 +0200

    (tweak) use update/condition obejcts in updateList query

--- backend/graphql/resolvers/list/update-list.ts
+++ backend/graphql/resolvers/list/update-list.ts
@@ -1,9 +1,19 @@
 import { sql } from "../../../db/init";
+import { List } from "../../types/List";
 
 // Only current usecase is updating list names, so let's implement it as such,
 // for now
 
-// TODO: trycatch, typing
+/**
+ * Update the name of a list..
+ * @note `list_id` should be unique, so we shouldn't need to involve e.g.
+ * `user_id`.
+ */
 export async function updateListName(list_id: number, payload: { name: string }) {
-   return await sql`update lists set name = ${payload.name} where list_id = ${list_id}`;
+   const update = { name: payload.name };
+   const condition = { list_id };
+
+   return await sql<[List?]>`update lists set ${sql(update)} where ${sql(
+      condition
+   )} returning *`;
 }
