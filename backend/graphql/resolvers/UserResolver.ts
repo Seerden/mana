d8046824a9f9f0bad80151a48daed689df034586
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-14 02:21:28 +0200

    (typegraphql) include new (some Authorized) query/mutation functions in UserResolver and update return types where needed

--- backend/graphql/resolvers/UserResolver.ts
+++ backend/graphql/resolvers/UserResolver.ts
@@ -1,48 +1,81 @@
-// @ts-nocheck
-
 import { ExpressContext } from "apollo-server-express";
-import { Arg, Ctx, Field, Mutation, ObjectType, Query, Resolver } from "type-graphql";
+import {
+   Arg,
+   Authorized,
+   Ctx,
+   Field,
+   Mutation,
+   ObjectType,
+   Query,
+   Resolver,
+} from "type-graphql";
+import { destroySession } from "../../lib/destroy-session";
+import { Roles } from "../helpers/roles";
 import { User } from "../types/User";
 import { createUser } from "./user/create-user";
 import { login } from "./user/login";
 import { queryAllUsers } from "./user/query-all-users";
 import { queryMe } from "./user/query-me";
+import { updatePassword, updateUsername } from "./user/update-user";
 
 @ObjectType()
-class MaybeUser {
-   @Field({ nullable: true })
-   error?: string;
-
-   @Field(() => User, { nullable: true })
-   user?: User;
+class Message {
+   @Field()
+   message: string;
 }
 
 @Resolver()
 export class UserResolver {
    @Query(() => [User], { name: "users" })
+   @Authorized([Roles.ADMIN])
    async users() {
-      return await queryAllUsers();
+      return queryAllUsers();
    }
 
-   @Query(() => MaybeUser)
+   @Query(() => User)
+   @Authorized()
    async me(@Ctx() { req }: ExpressContext) {
-      return await queryMe(req);
+      return queryMe(req);
    }
 
-   @Mutation(() => MaybeUser)
+   @Mutation(() => User)
    async createUser(
       @Arg("username") username: string,
       @Arg("password") password: string
-   ): Promise<MaybeUser> {
-      return await createUser(username, password);
+   ) {
+      return createUser(username, password);
    }
 
-   @Mutation(() => MaybeUser)
+   @Mutation(() => User)
    async login(
       @Arg("username") username: string,
       @Arg("password") password: string,
-      @Ctx() { req, res }: ExpressContext
-   ): Promise<MaybeUser> {
-      return await login(username, password, { req, res });
+      @Ctx() ctx: ExpressContext
+   ) {
+      return login(username, password, ctx);
+   }
+
+   @Mutation(() => Message)
+   async logout(@Ctx() ctx: ExpressContext) {
+      return destroySession(ctx);
+   }
+
+   @Mutation(() => User, { description: "Update a user's username" })
+   @Authorized()
+   async updateUsername(
+      @Arg("user_id") user_id: number,
+      @Arg("username")
+      username: string
+   ) {
+      return updateUsername(user_id, username);
+   }
+
+   @Mutation(() => User, { description: "Update a user's password" })
+   @Authorized()
+   async updatePassword(
+      @Arg("user_id") user_id: number,
+      @Arg("password") password: string
+   ) {
+      return updatePassword(user_id, password);
    }
 }
