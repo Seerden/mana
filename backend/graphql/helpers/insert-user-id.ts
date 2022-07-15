4c54a02c87f4d584b406906a9d304f2c8d1ff355
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-15 22:46:02 +0200

    (tweak) throw from UserId decorator if no active session

--- backend/graphql/helpers/insert-user-id.ts
+++ backend/graphql/helpers/insert-user-id.ts
@@ -1,8 +1,12 @@
-import { ExpressContext } from "apollo-server-express";
+import { AuthenticationError, ExpressContext } from "apollo-server-express";
 import { createParamDecorator } from "type-graphql";
 
 export function UserId() {
    return createParamDecorator<ExpressContext>(({ context }) => {
-      return context.req.session?.userId;
+      const { user_id } = context.req.session;
+
+      if (!user_id) throw new AuthenticationError("No active session.");
+
+      return user_id;
    });
 }
