524677b19e3700627027c534d52e477599b3c1f5
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-15 12:39:28 +0200

    (typegraphql) split up NewTerm into NewTermWithoutListId and NewTerm

--- backend/graphql/types/input_types/term.ts
+++ backend/graphql/types/input_types/term.ts
@@ -1,13 +1,10 @@
 import { Field, InputType, Int, ObjectType } from "type-graphql";
 
-@ObjectType("NewTerm")
-@InputType("NewTermInput")
-export class NewTerm {
+@ObjectType()
+@InputType()
+export class NewTermWithoutListId {
    @Field(() => Int)
    user_id: number;
-
-   @Field(() => Int)
-   list_id: number;
 
    @Field()
    from_language: string;
@@ -20,4 +17,11 @@ export class NewTerm {
 
    @Field()
    to_value: string;
+}
+
+@ObjectType("NewTerm")
+@InputType("NewTermInput")
+export class NewTerm extends NewTermWithoutListId {
+   @Field(() => Int)
+   list_id: number;
 }
