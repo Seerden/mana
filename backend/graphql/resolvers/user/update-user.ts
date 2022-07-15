9e241ac1d3d11acc38369bb28a73704ba7391e3c
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-15 11:12:58 +0200

    (fix) make updatePassword take current password and new password as args; (feat) implement verifyPassword helper function; (fix) update sql queries not to use 'condition' objects since syntax is incorrect

--- backend/graphql/resolvers/user/update-user.ts
+++ backend/graphql/resolvers/user/update-user.ts
@@ -1,22 +1,32 @@
 import { AuthenticationError, UserInputError } from "apollo-server-express";
-import { hash } from "bcryptjs";
+import { compare, hash } from "bcryptjs";
 import { sql } from "../../../db/init";
 import { User } from "../../types/User";
 
-/**
- * Update a user's password.
- */
-export async function updatePassword(user_id: number, password: string) {
-   if (!password?.length) throw new UserInputError("No password supplied");
+async function verifyPassword(user_id: number, password: string) {
+   const [user] = await sql<[User?]>`select * from users where user_id=${user_id}`;
+
+   return compare(password, user.password);
+}
+
+/** Update a user's password. */
+export async function updatePassword(
+   user_id: number,
+   currentPassword: string,
+   newPassword: string
+) {
+   if (!newPassword?.length) throw new UserInputError("No password supplied");
+
+   if (!(await verifyPassword(user_id, currentPassword)))
+      throw new Error("Password does not match database record.");
 
    const update = {
-      password: await hash(password, 10),
+      password: await hash(newPassword, 10),
    };
-   const condition = { user_id };
 
-   const [user] = await sql<[User?]>`update users set ${sql(update)} where ${sql(
-      condition
-   )} returning *`;
+   const [user] = await sql<[User?]>`update users set ${sql(
+      update
+   )} where user_id=${user_id} returning *`;
 
    return user;
 }
@@ -35,11 +45,10 @@ export async function updateUsername(user_id: number, username: string) {
    if (!username?.length) throw new UserInputError("No username supplied.");
 
    const update = { username };
-   const condition = { user_id };
 
-   const [user] = await sql<[User?]>`update users set ${sql(update)} where ${sql(
-      condition
-   )} returning *`;
+   const [user] = await sql<[User?]>`update users set ${sql(
+      update
+   )} where user_id=${user_id} returning *`;
 
    return user;
 }
