4e5c24996dab8c3b5c69b3129b8b5cf44e8a2c2b
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-16 15:24:03 +0200

    (test:database) test createUser()

---
+++ backend/graphql/resolvers/user/create-user.test.ts
@@ -0,0 +1,33 @@
+import { compare } from "bcryptjs";
+import { sql } from "../../../db/init";
+import { clearDatabaseData } from "../../../db/queries/clear-data";
+import { resetSequences } from "../../../db/queries/reset-sequences";
+import { User } from "../../types/User";
+import { createUser } from "./create-user";
+
+beforeAll(async () => {
+   await resetSequences();
+});
+
+afterAll(async () => {
+   await resetSequences();
+   await clearDatabaseData();
+});
+
+describe("createUser", () => {
+   it("insert a user", async () => {
+      const data: Pick<User, "username" | "password"> = {
+         password: "test_password",
+         username: "test_user",
+      };
+
+      await sql.begin(async (sql) => {
+         const user = await createUser({ sql, ...data });
+         expect(user.username).toEqual("test_user");
+         const passwordsMatch = await compare(data.password, user.password);
+         expect(passwordsMatch).toBeTruthy();
+
+         await sql`rollback`;
+      });
+   });
+});
