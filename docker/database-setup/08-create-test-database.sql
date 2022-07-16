fb5429ba0527e83dd01337f5038b9b0c8e810af9
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-16 15:24:52 +0200

    (wip:chore) delete create-test-database script since we use a separate service now

--- docker/database-setup/08-create-test-database.sql
+++
@@ -1,8 +0,0 @@
--- see https://stackoverflow.com/a/23611833/12820947
-
-/* KILL ALL EXISTING CONNECTION FROM ORIGINAL DB (mana)*/
-SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity 
-WHERE pg_stat_activity.datname = 'mana' AND pid <> pg_backend_pid();
-
-/* CLONE DATABASE TO NEW ONE(mana_test) */
-CREATE DATABASE mana_test WITH TEMPLATE mana OWNER postgres; -- Is there an easy way to inject environment variables into a .sql file?
