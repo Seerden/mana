bc0d297eac9b778d03a0e4c75eacb626fb4bb3e8
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-21 11:39:39 +0200

    (wip:refactor) adapt user mutations, queries to new shape

--- frontend/src/gql/operations/user-operations.ts
+++ frontend/src/gql/operations/user-operations.ts
@@ -1,12 +1,30 @@
 import { gql } from "graphql-request";
 
-export const registerUserMutation = gql`
-mutation ($username: String!, $password: String!) {
-    createUser(username: $username, password: $password) {
-        error 
-        user {
-            username
-        }
-    }
-}
-`
+export const createUserMutation = gql`
+	mutation ($username: String!, $password: String!) {
+		createUser(username: $username, password: $password) {
+			user_id
+			username
+		}
+	}
+`;
+
+export const queryMeMutation = gql`
+	query {
+		me {
+			user_id
+			username
+			created_at
+		}
+	}
+`;
+
+export const loginMutation = gql`
+	mutation ($password: String!, $username: String!) {
+		login(password: $password, username: $username) {
+			user_id
+			username
+			created_at
+		}
+	}
+`;
