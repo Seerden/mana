0908932c399c912649bfb048ffcbd6e3d0b4d504
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-26 00:25:37 +0200

    (refactor) use termFilter atom instead of local state, and adapt filterTermsBySaturation usage to new signature

--- frontend/src/components/list/hooks/useListFilter.ts
+++ frontend/src/components/list/hooks/useListFilter.ts
@@ -1,27 +1,23 @@
 import { filterTermsBySaturation } from "components/list/helpers/filterTermsBySaturation";
-import { FilterInterface } from "components/list/types/list.types";
 import { useQueryListsById } from "gql/hooks/list/useQueryLists";
 import useRouteProps from "hooks/useRouteProps";
-import { useMemo, useState } from "react";
-
-const defaultFilter: FilterInterface = {
-	saturation: { level: undefined, direction: "any" },
-};
+import { useMemo } from "react";
+import { useRecoilValue } from "recoil";
+import { termFilterState } from "../../../state/filter";
 
 export function useListFilter() {
 	const { params } = useRouteProps();
 	const { data: lists } = useQueryListsById([+params.id]);
-	const [filter, setFilter] = useState<FilterInterface>(defaultFilter);
+	const termFilter = useRecoilValue(termFilterState);
 
 	const visibleTermIds = useMemo(() => {
 		if (!lists[0]?.terms) return [];
 
-		return filterTermsBySaturation(filter, lists[0].terms);
-	}, [lists[0].terms, filter]);
+		return filterTermsBySaturation(termFilter, lists[0].terms);
+	}, [lists[0].terms, termFilter]);
 
 	return {
-		filter,
-		setFilter,
+		termFilter,
 		visibleTermIds,
 	} as const;
 }
