8dfbcab634c001e633a564287ffb1c83a60fd1a2
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-25 16:30:09 +0200

    (wip:refactor) comment out filterTermsBySaturation logic pending rework

--- frontend/src/components/list/helpers/filterTermsBySaturation.ts
+++ frontend/src/components/list/helpers/filterTermsBySaturation.ts
@@ -1,26 +1,29 @@
-import { FilterInterface, TruncatedTerm } from "components/list/types/list.types";
+import { FilterInterface } from "components/list/types/list.types";
+import { Term } from "../../../gql/codegen-output";
 
-export function filterTermsBySaturation(filter: FilterInterface, terms: TruncatedTerm[]) {
+export function filterTermsBySaturation(
+	filter: FilterInterface,
+	terms: Term[]
+): Term["term_id"][] {
+	return terms.map((t) => t.term_id);
+
 	if (!terms?.length) return [];
+	if (!filter.saturation?.level) return terms.map((t) => t.term_id);
 
-	return terms?.filter((term) => {
-		if (filter.saturation?.level) {
-			if (!term.saturation) {
-				return true;
-			}
+	return terms
+		.filter((term) => {
+			if (!term.saturation) return true;
 
 			if (filter.saturation.direction !== "any") {
 				return (
-					term.saturation?.[filter.saturation.direction] ===
+					+term.saturation?.[filter.saturation.direction] ===
 					Number(filter?.saturation.level)
 				);
 			}
-			return (
-				term.saturation?.forwards === Number(filter?.saturation.level) ||
-				term.saturation?.backwards === Number(filter?.saturation.level)
-			);
-		} else {
-			return true;
-		}
-	});
+
+			const { forwards, backwards } = term.saturation;
+			// TODO: temporarily cast to number pending refactor
+			return [forwards, backwards].includes(filter?.saturation.level as number);
+		})
+		.map((t) => t.term_id);
 }
