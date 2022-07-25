fd72d77112ac7309639876b19b635e405152d03f
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-25 23:11:29 +0200

    (wip:refactor) update filter types

--- frontend/src/components/SaturationFilter/types/filter-types.ts
+++ frontend/src/components/SaturationFilter/types/filter-types.ts
@@ -1,19 +1,21 @@
-import { operators } from "../helpers/operators";
-
-export type FilterValue = {
-	value?: number;
-	operator: `${operators}`;
-};
-
-export type ThresholdKey = "minimum" | "maximum";
-
-export type Threshold = {
-	[k in ThresholdKey]?: FilterValue;
-};
+/* eslint-disable no-mixed-spaces-and-tabs */
+import { Operator } from "../OperatorButton";
 
 // TODO: very reusable type, consider globally defining this.
 type Direction = "forwards" | "backwards";
 
+type FilterDirection = Direction | "any";
+
 export type TermFilter = {
-	[k in Direction]?: Threshold;
+	direction: FilterDirection;
+	operator: Operator;
+	value: number;
 };
+
+export type FilterUpdate =
+	| {
+			field: "value";
+			value: number;
+	  }
+	| { field: "direction"; value: TermFilter["direction"] }
+	| { field: "operator"; value: Operator };
