3a5783000efd3b217399887078141b81fccbc8b7
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-25 12:01:37 +0200

    (tweak) don't throw an error from UserId decorator if no user in session

--- backend/graphql/helpers/insert-user-id.ts
+++ backend/graphql/helpers/insert-user-id.ts
@@ -1,11 +1,11 @@
-import { AuthenticationError, ExpressContext } from "apollo-server-express";
+import { ExpressContext } from "apollo-server-express";
 import { createParamDecorator } from "type-graphql";
 
 export function UserId() {
    return createParamDecorator<ExpressContext>(({ context }) => {
       const { user_id } = context.req.session;
 
-      if (!user_id) throw new AuthenticationError("No active session.");
+      if (!user_id) return; // TODO: Consider throwing an error again once we have proper global error handling client-side.
 
       return user_id;
    });
