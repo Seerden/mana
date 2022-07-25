1fafe6d9abf488f57c9b21d9877e4c0751023527
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-25 23:47:49 +0200

    (refactor:useSaturationFilter) use subcomponents instead of defining button elements inline; deduplicate setFilter handler, don't use two types of filter state

--- frontend/src/components/SaturationFilter/hooks/useSaturationFilter.tsx
+++ frontend/src/components/SaturationFilter/hooks/useSaturationFilter.tsx
@@ -1,81 +1,118 @@
-import { FilterInterface } from "components/list/types/list.types";
 import { colorMap } from "helpers/list.api";
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import { BiArrowToLeft, BiArrowToRight } from "react-icons/bi";
+import { useRecoilState, useResetRecoilState } from "recoil";
+import { termFilterState } from "../../../state/filter";
+import OperatorButton, { Operator } from "../FilterButtons";
+import { operators } from "../helpers/operators";
 import * as S from "../SaturationFilter.style";
+import { FilterUpdate } from "../types/filter-types";
 
-export function useSaturationFilter(setFilter) {
-	const [saturationFilter, setSaturationFilter] = useState<
-		FilterInterface["saturation"]
-	>({
-		level: undefined,
-		direction: "any",
-	});
-	const [filterDisplayState, setFilterDisplayState] = useState("initial");
+export enum FilterStep {
+	INITIAL = 0,
+	DIRECTION = 1,
+	OPERATOR = 2,
+	LEVEL = 3,
+}
 
-	useEffect(() => {
-		setFilter((cur) => ({ ...cur, saturation: saturationFilter }));
-	}, [saturationFilter, setFilter]);
+export function useSaturationFilter() {
+	const [termFilter, setTermFilter] = useRecoilState(termFilterState);
+	const resetTermFilter = useResetRecoilState(termFilterState);
 
-	function handleIconClick(level) {
-		setSaturationFilter((cur) => ({ ...cur, level }));
-		setFilterDisplayState("direction");
+	const [filterStep, setFilterStep] = useState<number>(FilterStep.INITIAL);
+
+	/**
+	 * Cycle to the next filter step.
+	 * @usage only called from handleFilterButtonClick after updating filter
+	 * state.
+	 */
+	function cycleFilterStep() {
+		// Cycle through domain [0,n) where n === Object.values(FilterStep).length
+		setFilterStep((cur) => (cur + 1) % 4 ?? FilterStep.INITIAL);
 	}
 
-	function handleDirectionIconClick(direction) {
-		setSaturationFilter((cur) => ({ ...cur, direction }));
-		setFilterDisplayState("initial");
+	/** Update filter state and move on to next filter step. */
+	function handleFilterButtonClick({ field, value }: FilterUpdate) {
+		setTermFilter((cur) => ({ ...cur, [field]: value }));
+		cycleFilterStep();
 	}
 
-	function makeIcons() {
-		const icons = Object.keys(colorMap).map((level) => {
-			const levelNumber = Number(level);
-			return (
-				<React.Fragment key={`saturation-wrapper-${level}`}>
-					<S.IconWrapper
-						onClick={() => {
-							handleIconClick(level);
-						}}
-					>
-						<S.FilterIcon
-							key={`saturation-filter-level-${level}`}
-							// TODO: temporarily fake saturation level since it only
-							// takes {forwards, backwards} now, and no longer allows
-							// number type.
-							saturation={{ forwards: levelNumber, backwards: levelNumber }}
-						/>
-					</S.IconWrapper>
-				</React.Fragment>
-			);
-		});
-		return icons;
-	}
-
-	const icons = makeIcons();
-
-	const directionButtons = ["forwards", "backwards", "any"].map((direction) => {
+	const thresholdButtons = Object.keys(colorMap).map((level) => {
 		return (
-			<S.DirectionButton
-				key={`saturation-filter-${direction}`}
-				value={direction}
-				onClick={() => handleDirectionIconClick(direction)}
-			>
-				<span>{direction}</span>
-				{direction !== "any" && (
-					<span>
-						{direction === "forwards" ? <BiArrowToRight /> : <BiArrowToLeft />}
-					</span>
-				)}
-			</S.DirectionButton>
+			<SaturationThresholdButton
+				level={+level}
+				key={`saturation-wrapper-${level}`}
+				onClick={() => {
+					handleFilterButtonClick({ field: "value", value: +level });
+				}}
+			/>
 		);
 	});
 
+	const directionButtons = ["forwards", "backwards", "any"].map(
+		(direction: Direction | "any") => {
+			return (
+				<DirectionButton
+					key={direction}
+					direction={direction}
+					onClick={() =>
+						handleFilterButtonClick({ field: "direction", value: direction })
+					}
+				/>
+			);
+		}
+	);
+
+	const operatorButtons = Object.values(operators).map((operator: Operator) => (
+		<OperatorButton
+			key={operator}
+			operator={operator}
+			onClick={() => handleFilterButtonClick({ field: "operator", value: operator })}
+		/>
+	));
+
 	return {
-		filterDisplayState,
-		setFilterDisplayState,
-		saturationFilter,
-		setSaturationFilter,
-		icons,
+		filterStep,
+		cycleFilterStep,
+		termFilter,
+		resetTermFilter,
+		operatorButtons,
+		thresholdButtons,
 		directionButtons,
 	} as const;
+}
+
+type ThresholdButtonProps = {
+	level: number;
+	onClick?: (...args: any) => void;
+};
+
+function SaturationThresholdButton({ level, onClick }: ThresholdButtonProps) {
+	return (
+		<S.IconWrapper onClick={() => onClick?.()}>
+			<S.FilterIcon saturation={level} />
+		</S.IconWrapper>
+	);
+}
+
+type DirectionButtonProps = {
+	direction: Direction | "any";
+	onClick?: (...args: any) => void;
+};
+
+export function DirectionButton({ direction, onClick }: DirectionButtonProps) {
+	return (
+		<S.DirectionButton
+			key={`saturation-filter-${direction}`}
+			value={direction}
+			onClick={() => onClick?.(direction)}
+		>
+			<span>{direction}</span>
+			{direction !== "any" && (
+				<span>
+					{direction === "forwards" ? <BiArrowToRight /> : <BiArrowToLeft />}
+				</span>
+			)}
+		</S.DirectionButton>
+	);
 }
