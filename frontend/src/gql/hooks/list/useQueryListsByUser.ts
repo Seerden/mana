c547a2b4378f2d3d880d1f64d5f4d0e95e3c0069
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-25 12:19:56 +0200

    (fix:useQueryListsByUser) don't pass user_id from client; use requestClient

--- frontend/src/gql/hooks/list/useQueryListsByUser.ts
+++ frontend/src/gql/hooks/list/useQueryListsByUser.ts
@@ -1,18 +1,19 @@
-import request, { gql } from "graphql-request";
+import { gql } from "graphql-request";
 import { useQuery } from "react-query";
-import { uri } from "../../../helpers/graphql-uri";
-import { List, User } from "../../codegen-output";
+import requestClient from "../../../components/newlist/helpers/request-client";
+import { List } from "../../codegen-output";
+import { listPropsFragment } from "../../fragments/list-fragments";
+import { termPropsFragment } from "../../fragments/term-fragments";
 
 /**
- * TODO: Check if TermProps and ListProps fragments work without being
- * specifically called using `${listPropsFragment}` etc.
- *
  * NOTE: TermProps fragment does not by default include `term.saturation`.
  */
 
 const query = gql`
-	query ($user_id: Float!) {
-		listsByUser(user_id: $user_id) {
+	${listPropsFragment}
+	${termPropsFragment}
+	query {
+		listsByUser {
 			...ListProps
 			terms(populate: true) {
 				...TermProps
@@ -21,8 +22,8 @@ const query = gql`
 	}
 `;
 
-const req = (user_id: User["user_id"]) => request(uri, query, { user_id });
+const req = async () => (await requestClient.request(query)).listsByUser;
 
-export default function useQueryListsByUser(user_id: User["user_id"]) {
-	return useQuery<List[]>(["lists"], () => req(user_id));
+export default function useQueryListsByUser() {
+	return useQuery<List[]>(["lists"], () => req());
 }
