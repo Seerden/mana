717984da5e001abb9e280eb132329a356a165d98
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-25 16:12:57 +0200

    (type) update newList atom typing to work with new typ definitions from backend

--- frontend/src/components/newlist/state/newList.atom.ts
+++ frontend/src/components/newlist/state/newList.atom.ts
@@ -1,13 +1,12 @@
-import { NewList } from "components/newlist/types/newList.types";
 import { atom } from "recoil";
+import { NewListWithTermsInput } from "../../../gql/codegen-output";
 
 export const newListState = atom({
 	key: "newListState",
 	default: {
-		owner: "",
-		from: "",
-		to: "",
+		from_language: "",
+		to_language: "",
 		name: "",
 		terms: [],
-	} as NewList,
+	} as NewListWithTermsInput,
 });
