5d55d9c3959c02d13ba944c7a7fdda78abf93269
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-25 12:25:14 +0200

    (cleanup:type) remove unused client-side defined NewList type

--- frontend/src/components/newlist/types/newList.types.ts
+++ frontend/src/components/newlist/types/newList.types.ts
@@ -1,13 +1,3 @@
-import { NewListTermInput } from "gql/codegen-output";
-
-export type NewList = {
-	owner?: string;
-	name?: string;
-	from?: string;
-	to?: string;
-	terms?: NewListTermInput[];
-};
-
 export type FocusIndex = {
 	index: number;
 	side?: "from" | "to";
