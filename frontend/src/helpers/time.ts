877e435cce4cf4073a0c52d18706d695346b464d
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-25 16:25:02 +0200

    (type) allow 'date' arg to have number type

--- frontend/src/helpers/time.ts
+++ frontend/src/helpers/time.ts
@@ -10,8 +10,8 @@ dayjs.extend(duration);
  * Return time to or from the given date
  * @returns {string} e.g. '2 days ago'
  */
-export function timeSince(date: Date | null): string | null {
-    return date ? dayjs(date).fromNow() : null;
+export function timeSince(date: number | Date | null): string | null {
+	return date ? dayjs(date).fromNow() : null;
 }
 
 /**
@@ -19,15 +19,15 @@ export function timeSince(date: Date | null): string | null {
  * @param format dayjs parse format
  */
 export function formatDate(date: Date, format: string): string {
-    // @todo: figure out dayjs types
-    return dayjs(date).format(format);
+	// @todo: figure out dayjs types
+	return dayjs(date).format(format);
 }
 
 export type Timescale = "minutes" | "hours" | "seconds";
 
 export function dateDifference(date1: Date, date2: Date, timescale: Timescale) {
-    return dayjs(date1).diff(dayjs(date2), timescale);
+	return dayjs(date1).diff(dayjs(date2), timescale);
 }
 
 export const humanizedDateDifference = (date1: Date, date2: Date) =>
-    dayjs.duration(dayjs(date1).diff(date2)).humanize();
+	dayjs.duration(dayjs(date1).diff(date2)).humanize();
