e647fbb1e039a424ab8f31773e275a5caf611318
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-27 17:33:39 +0200

    (tweak) don't memoize NewListTerm

--- frontend/src/components/newlist/sub/NewListTerm.tsx
+++ frontend/src/components/newlist/sub/NewListTerm.tsx
@@ -1,6 +1,6 @@
 import { newListState } from "components/newlist/state/newList.atom";
 import type { FocusIndex } from "components/newlist/types/newList.types";
-import React, { memo, useMemo } from "react";
+import React, { useMemo } from "react";
 import { useSetRecoilState } from "recoil";
 import * as S from "./NewListTerm.style";
 
@@ -11,60 +11,63 @@ type NewListTermProps = {
 	autoFocus: boolean;
 };
 
-const NewListTerm = memo(
-	({ index, setFocussedInput, focussedInput, autoFocus }: NewListTermProps) => {
-		const setNewList = useSetRecoilState(newListState);
-		const isFocussed = useMemo(() => focussedInput?.index === index, [focussedInput]);
+function NewListTerm({
+	index,
+	setFocussedInput,
+	focussedInput,
+	autoFocus,
+}: NewListTermProps) {
+	const setNewList = useSetRecoilState(newListState);
+	const isFocussed = useMemo(() => focussedInput?.index === index, [focussedInput]);
 
-		function handleTermBlur(e: React.FocusEvent<HTMLInputElement>, idx: number) {
-			setFocussedInput((cur) => ({ ...cur, index: -1 }));
-			const { name, value } = e.target; // name is "from_value" | "to_value"
+	function handleTermBlur(e: React.FocusEvent<HTMLInputElement>, idx: number) {
+		setFocussedInput((cur) => ({ ...cur, index: -1 }));
+		const { name, value } = e.target; // name is "from_value" | "to_value"
 
-			if (!value) return;
+		if (!value) return;
 
-			setNewList((cur) => {
-				const termsCopy = cur.terms.slice();
+		setNewList((cur) => {
+			const termsCopy = cur.terms.slice();
 
-				if (!termsCopy[idx]) {
-					termsCopy[idx] = {
-						to_value: "",
-						from_value: "",
-						from_language: "",
-						to_language: "",
-					};
-				}
+			if (!termsCopy[idx]) {
+				termsCopy[idx] = {
+					to_value: "",
+					from_value: "",
+					from_language: "",
+					to_language: "",
+				};
+			}
 
-				if (value !== termsCopy[idx][name]) {
-					termsCopy[idx] = { ...termsCopy[idx], [name]: value };
-					return { ...cur, terms: termsCopy };
-				}
+			if (value !== termsCopy[idx][name]) {
+				termsCopy[idx] = { ...termsCopy[idx], [name]: value };
+				return { ...cur, terms: termsCopy };
+			}
 
-				return cur;
-			});
-		}
-
-		const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
-			e.persist();
-			setFocussedInput({ side: e.currentTarget.name, index } as FocusIndex);
-		};
-
-		const inputProps: Partial<React.InputHTMLAttributes<HTMLInputElement>> = {
-			onFocus: (e) => handleFocus(e),
-			onBlur: (e) => handleTermBlur(e, index),
-			type: "text",
-		};
-
-		return (
-			<S.Term>
-				<S.TermIndex isFocussed={isFocussed}>{index + 1}</S.TermIndex>
-
-				<S.TermInputs>
-					<S.TermInput {...inputProps} autoFocus={autoFocus} name="from_value" />
-					<S.TermInput {...inputProps} name="to_value" />
-				</S.TermInputs>
-			</S.Term>
-		);
+			return cur;
+		});
 	}
-);
+
+	const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+		e.persist();
+		setFocussedInput({ side: e.currentTarget.name, index } as FocusIndex);
+	};
+
+	const inputProps: Partial<React.InputHTMLAttributes<HTMLInputElement>> = {
+		onFocus: (e) => handleFocus(e),
+		onBlur: (e) => handleTermBlur(e, index),
+		type: "text",
+	};
+
+	return (
+		<S.Term>
+			<S.TermIndex isFocussed={isFocussed}>{index + 1}</S.TermIndex>
+
+			<S.TermInputs>
+				<S.TermInput {...inputProps} autoFocus={autoFocus} name="from_value" />
+				<S.TermInput {...inputProps} name="to_value" />
+			</S.TermInputs>
+		</S.Term>
+	);
+}
 
 export default NewListTerm;
