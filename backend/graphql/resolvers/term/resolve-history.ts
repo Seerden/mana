262ff0fa010063a9f7e15ad0167feb2d280fe95f
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-27 16:29:55 +0200

    (refactor) adjust term.history resolver function to return a list of lists of ReviewSessionEntry

--- backend/graphql/resolvers/term/resolve-history.ts
+++ backend/graphql/resolvers/term/resolve-history.ts
@@ -13,7 +13,11 @@ type Args = {
 export async function resolveTermHistory({ term, populate, sql = instance }: Args) {
    if (!populate) return;
 
-   return sql<[ReviewSessionEntry]>`
-      select * from review_session_entries where term_id=${term.term_id}
-   `;
+   return (
+      await sql<[{ session: [ReviewSessionEntry] }]>`
+      select jsonb_agg(to_jsonb(r.*)) session from (
+         select e.*, extract(epoch from e.created_at) as created_at from review_session_entries e where term_id=${term.term_id}
+      ) r group by r.review_session_id
+   `
+   ).map((x) => x.session);
 }
