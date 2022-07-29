eafc57c93a1d988c0916c255350953ff124b10ac
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-29 23:16:14 +0200

    (fix) prevent submission of newList without languages or name

--- frontend/src/components/newlist/helpers/validate-new-list.ts
+++ frontend/src/components/newlist/helpers/validate-new-list.ts
@@ -4,13 +4,11 @@ import { filterFalsy } from "./filterFalsyValues";
 export function isValidNewList(newList: NewListWithTermsInput) {
 	const fields = ["name", "from_language", "to_language"];
 
-	const fieldsAreValid = fields.every(
-		(entry) =>
-			entry in newList &&
-			(typeof newList[entry] == "string" || Array.isArray(newList[entry]))
-	);
+	for (const field of fields) {
+		if (!newList[field]?.length) return false;
+	}
 
 	const hasTerms = filterFalsy(newList.terms)?.length > 0;
 
-	return fieldsAreValid && hasTerms;
+	return !!hasTerms;
 }
