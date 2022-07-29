b1f1a343358106e4803e98f2ce0f3617dddd89cb
Christiaan Seerden <christiaanseerden@live.nl>
2022-07-29 23:19:29 +0200

    (chore) remove unnecessary div wrapper

--- frontend/src/components/newlist/NewList.tsx
+++ frontend/src/components/newlist/NewList.tsx
@@ -6,68 +6,66 @@ function NewList() {
 		useNewList();
 
 	return (
-		<div>
-			<S.Form>
-				<S.Title style={{ gridArea: "title" }}>New List</S.Title>
-				<S.MetaSection>
-					<S.NameSubsection>
-						<S.SectionLabel htmlFor="name">List name</S.SectionLabel>
-						<S.NameInput
-							onChange={handleBlur}
-							type="text"
-							name="name"
-							placeholder="Chapter 3: Basic Sentences"
-						/>
-					</S.NameSubsection>
+		<S.Form>
+			<S.Title style={{ gridArea: "title" }}>New List</S.Title>
+			<S.MetaSection>
+				<S.NameSubsection>
+					<S.SectionLabel htmlFor="name">List name</S.SectionLabel>
+					<S.NameInput
+						onChange={handleBlur}
+						type="text"
+						name="name"
+						placeholder="Chapter 3: Basic Sentences"
+					/>
+				</S.NameSubsection>
 
-					<div>
-						<S.SectionLabel>Languages</S.SectionLabel>
-						<S.Languages>
-							<div>
-								<S.LanguageLabel htmlFor="from_language">
-									Original language
-								</S.LanguageLabel>
-								<S.LanguageInput
-									onChange={handleBlur}
-									type="text"
-									name="from_language"
-									placeholder="Klingon"
-								/>
-							</div>
-							<S.LanguageIcon size={25} />
+				<div>
+					<S.SectionLabel>Languages</S.SectionLabel>
+					<S.Languages>
+						<div>
+							<S.LanguageLabel htmlFor="from_language">
+								Original language
+							</S.LanguageLabel>
+							<S.LanguageInput
+								onChange={handleBlur}
+								type="text"
+								name="from_language"
+								placeholder="Klingon"
+							/>
+						</div>
+						<S.LanguageIcon size={25} />
 
-							<div>
-								<S.LanguageLabel htmlFor="to_language">
-									Target language
-								</S.LanguageLabel>
-								<S.LanguageInput
-									onChange={handleBlur}
-									type="text"
-									name="to_language"
-									placeholder="Elvish"
-								/>
-							</div>
-						</S.Languages>
-					</div>
-				</S.MetaSection>
+						<div>
+							<S.LanguageLabel htmlFor="to_language">
+								Target language
+							</S.LanguageLabel>
+							<S.LanguageInput
+								onChange={handleBlur}
+								type="text"
+								name="to_language"
+								placeholder="Elvish"
+							/>
+						</div>
+					</S.Languages>
+				</div>
+			</S.MetaSection>
 
-				<S.Buttons style={{ gridArea: "buttons" }} ref={buttonsRef} isStuck={isStuck}>
-					<S.Button onClick={() => addRows()} type="button" value="Add rows" />
-					<S.Button onClick={handleSubmit} type="button" value="Create list" />
-				</S.Buttons>
+			<S.Buttons style={{ gridArea: "buttons" }} ref={buttonsRef} isStuck={isStuck}>
+				<S.Button onClick={() => addRows()} type="button" value="Add rows" />
+				<S.Button onClick={handleSubmit} type="button" value="Create list" />
+			</S.Buttons>
 
-				<S.Section style={{ gridArea: "terms" }}>
-					<S.SectionLabel>Terms</S.SectionLabel>
-					{newList.from_language && newList.to_language && (
-						<S.TermsHeader>
-							<S.TermsHeaderSide>{`${newList.from_language} definition`}</S.TermsHeaderSide>
-							<S.TermsHeaderSide>{`${newList.to_language} definition`}</S.TermsHeaderSide>
-						</S.TermsHeader>
-					)}
-					{termInputs}
-				</S.Section>
-			</S.Form>
-		</div>
+			<S.Section style={{ gridArea: "terms" }}>
+				<S.SectionLabel>Terms</S.SectionLabel>
+				{newList.from_language && newList.to_language && (
+					<S.TermsHeader>
+						<S.TermsHeaderSide>{`${newList.from_language} definition`}</S.TermsHeaderSide>
+						<S.TermsHeaderSide>{`${newList.to_language} definition`}</S.TermsHeaderSide>
+					</S.TermsHeader>
+				)}
+				{termInputs}
+			</S.Section>
+		</S.Form>
 	);
 }
 
