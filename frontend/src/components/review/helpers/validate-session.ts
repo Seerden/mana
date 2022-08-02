3e4dc65c48dc851de85c4a92bf9876b8108c7b1a
Christiaan Seerden <christiaanseerden@live.nl>
2022-08-02 12:07:48 +0200

    (feat) create session validator helpers

---
+++ frontend/src/components/review/helpers/validate-session.ts
@@ -0,0 +1,25 @@
+import { ReviewSessionInput } from "../../../gql/codegen-output";
+
+/** Type guard to see if a string is a valid direection. */
+export function isDirection(s: string): s is Direction {
+	return ["forwards", "backwards"].includes(s);
+}
+
+/** Check if all of a reviewSession's required fields are valid. */
+export function isValidReviewSession(session: Partial<ReviewSessionInput>) {
+	const { direction, start_date, n, list_ids, set_ids, saturation_threshold } = session;
+
+	if (
+		!list_ids?.length &&
+		!set_ids?.length &&
+		!(typeof saturation_threshold === "number")
+	)
+		return false;
+
+	return (
+		isDirection(direction) &&
+		typeof n === "number" &&
+		n > 0 &&
+		typeof start_date === "number"
+	);
+}
