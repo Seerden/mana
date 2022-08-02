a49ee4a2f01440a0d38c43d48e0bd61e7a4269a7
Christiaan Seerden <christiaanseerden@live.nl>
2022-08-02 12:32:01 +0200

    (type) type useLogState parameters

--- frontend/src/hooks/useLogState.ts
+++ frontend/src/hooks/useLogState.ts
@@ -3,7 +3,11 @@ import { useEffect } from "react";
 /**
  * useEffect hook to log whenever a piece of state (or its initializer) changes
  */
-export const useLogState = (name, state, setState) => {
+export const useLogState = (
+	name: string,
+	state: unknown,
+	setState: (state: any) => void
+) => {
 	useEffect(() => {
 		if (state) {
 			console.log(`${name} changed:`, state);
