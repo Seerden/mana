13143bd364c1728c51f5313069a806fed88f417b
Christiaan <christchristiaanseerden@live.nl>
2022-08-03 22:51:12 +0200

    (state) add reviewEntriesState atom and update imported graphql types

--- frontend/src/components/review/state/review-session.ts
+++ frontend/src/components/review/state/review-session.ts
@@ -1,7 +1,12 @@
 import { atom } from "recoil";
-import { ReviewSessionInput } from "../../../gql/codegen-output";
+import { ReviewSessionEntryInput, ReviewSessionInput } from "../../../gql/codegen-output";
 
 export const reviewSessionState = atom<Partial<ReviewSessionInput>>({
 	default: {},
 	key: "reviewSessionState",
+});
+
+export const reviewEntriesState = atom<Partial<ReviewSessionEntryInput>[]>({
+	default: [],
+	key: "reviewEntries",
 });
