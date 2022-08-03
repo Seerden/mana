ff27d811a7cceb9541cef67b62a12d4b21b9c06f
Christiaan <christchristiaanseerden@live.nl>
2022-08-03 22:50:19 +0200

    (cleanup) remove unused extractSessionsByDirection(); (type) refine type of level as colorMap property accessor

--- frontend/src/helpers/list.api.ts
+++ frontend/src/helpers/list.api.ts
@@ -1,16 +1,5 @@
 type Direction = "forwards" | "backwards";
 
-/** Takes a list, returns all its sessions with the specified direction, and its
- * length.
- */
-export const extractSessionsByDirection = (list, direction: Direction) => {
-	// @todo: sessions currently don't exist as list.sessions entries.
-	const sessionsByDirection = list.sessions?.filter(
-		(sess) => sess.direction === direction
-	);
-	return [sessionsByDirection.length, sessionsByDirection];
-};
-
 export const colorMap = {
 	0: "orangered",
 	1: "goldenrod",
@@ -20,5 +9,5 @@ export const colorMap = {
 };
 
 export const colorBySaturation = (level: number) => {
-	return level >= 0 ? colorMap[level] : "#333";
+	return level >= 0 ? colorMap[level as keyof typeof colorMap] : "#333";
 };
