631dcfcf947f8dcb2b40e10124792c124ce2b1de
Christiaan <christchristiaanseerden@live.nl>
2022-08-03 22:53:44 +0200

    (type) add prop type to PageInfo component; (chore) autoformat PageInfo component

--- frontend/src/components/_shared/PageInfo.tsx
+++ frontend/src/components/_shared/PageInfo.tsx
@@ -1,34 +1,35 @@
 import { useToggle } from "hooks/useToggle";
+import { PropsWithChildren } from "react";
 import { AiFillInfoCircle } from "react-icons/ai";
 import "./style/PageInfo.scss";
 
-const PageInfo = ({ children }) => {
-    const [isOpen, toggleOpen] = useToggle(false);
+const PageInfo = (props: PropsWithChildren) => {
+	const [isOpen, toggleOpen] = useToggle(false);
 
-    return (
-        <div className="PageInfo">
-            <div className="PageInfo__wrapper">
-                <div
-                    className="PageInfo__button"
-                    style={{ color: isOpen ? "deepskyblue" : "white" }}
-                >
-                    {!isOpen ? (
-                        <AiFillInfoCircle
-                            size={25}
-                            className="PageInfo__icon"
-                            onClick={toggleOpen}
-                        />
-                    ) : (
-                        <button className="PageInfo__close" onClick={toggleOpen}>
-                            x
-                        </button>
-                    )}
-                </div>
+	return (
+		<div className="PageInfo">
+			<div className="PageInfo__wrapper">
+				<div
+					className="PageInfo__button"
+					style={{ color: isOpen ? "deepskyblue" : "white" }}
+				>
+					{!isOpen ? (
+						<AiFillInfoCircle
+							size={25}
+							className="PageInfo__icon"
+							onClick={toggleOpen}
+						/>
+					) : (
+						<button className="PageInfo__close" onClick={toggleOpen}>
+							x
+						</button>
+					)}
+				</div>
 
-                {isOpen && <div className="PageInfo__info">{children}</div>}
-            </div>
-        </div>
-    );
+				{isOpen && <div className="PageInfo__info">{props.children}</div>}
+			</div>
+		</div>
+	);
 };
 
 export default PageInfo;
