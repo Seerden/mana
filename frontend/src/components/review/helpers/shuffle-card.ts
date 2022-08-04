8b4d5aa1e49ec4ce80202aa12f4f97df9526fe9d
Christiaan <christchristiaanseerden@live.nl>
2022-08-04 11:35:02 +0200

    (tweak) return clone of terms in shuffleCurrentTerm and document reasoning

--- frontend/src/components/review/helpers/shuffle-card.ts
+++ frontend/src/components/review/helpers/shuffle-card.ts
@@ -3,10 +3,16 @@ import { Term } from "../../../gql/codegen-output";
 /**
  * Shuffle the first term back into the array at a random spot. If it's the
  * only remaining card, return (a copy of) the given array.
+ *
+ * @note In both cases, we take the created array and do .map(t => ({...t})).
+ * This is to create a sort-of deep clone (since Terms have at least some
+ * non-nested properties). This way, ReviewCard will think it got a different
+ * Term, even if maybe it's actually the same term in reality. This way we reset
+ * the internal state of ReviewCard.
  */
 export function shuffleCurrentTerm(terms: Term[]) {
 	if (terms.length === 1) {
-		return terms.slice();
+		return terms.map((term) => ({ ...term }));
 	}
 
 	const newIndex = Math.floor((terms.length + 1) * Math.random());
@@ -14,5 +20,5 @@ export function shuffleCurrentTerm(terms: Term[]) {
 	const currentTerm = termsCopy.shift();
 	termsCopy.splice(newIndex, 0, currentTerm);
 
-	return termsCopy;
+	return termsCopy.map((term) => ({ ...term }));
 }
