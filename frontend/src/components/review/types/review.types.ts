ec9272793a65dafd489d657d291fafe6f79571ff
Christiaan <christchristiaanseerden@live.nl>
2022-08-04 13:00:43 +0200

    (type) add SessionEntryWithoutTimeOnCard intermediate type

--- frontend/src/components/review/types/review.types.ts
+++ frontend/src/components/review/types/review.types.ts
@@ -1,2 +1,6 @@
+import { ReviewSessionEntryInput } from "../../../gql/codegen-output";
+
 export type PassFailArray = Array<"pass" | "fail">;
 export type ReviewStage = "before" | "started" | "after";
+
+export type SessionEntryWithoutTimeOnCard = Omit<ReviewSessionEntryInput, "time_on_card">;
