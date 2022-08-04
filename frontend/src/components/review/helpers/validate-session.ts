a1b29faa52e148546bb3a0905aa53cfeb3abf0d3
Christiaan <christchristiaanseerden@live.nl>
2022-08-04 02:12:29 +0200

    (fix:type) use new ReviewSessionWithoutUserIdInput type

--- frontend/src/components/review/helpers/validate-session.ts
+++ frontend/src/components/review/helpers/validate-session.ts
@@ -1,4 +1,4 @@
-import { ReviewSessionInput } from "../../../gql/codegen-output";
+import { ReviewSessionWithoutUserIdInput } from "../../../gql/codegen-output";
 
 /** Type guard to see if a string is a valid direection. */
 export function isDirection(s: string): s is Direction {
@@ -6,7 +6,7 @@ export function isDirection(s: string): s is Direction {
 }
 
 /** Check if all of a reviewSession's required fields are valid. */
-export function isValidReviewSession(session: Partial<ReviewSessionInput>) {
+export function isValidReviewSession(session: Partial<ReviewSessionWithoutUserIdInput>) {
 	const { direction, start_date, n, list_ids, set_ids, saturation_threshold } = session;
 
 	if (
