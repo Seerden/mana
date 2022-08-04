014a398a68219de5cdf1f517bf8e56f29fbc92a1
Christiaan <christchristiaanseerden@live.nl>
2022-08-04 02:10:17 +0200

    (typegraphql) split up ReviewSession types more to exclude user_id from Input; add SessionAndEntries type for createSession mutation

--- backend/graphql/types/ReviewSession.ts
+++ backend/graphql/types/ReviewSession.ts
@@ -1,11 +1,9 @@
 import { Field, Float, InputType, Int, ObjectType } from "type-graphql";
+import { ReviewSessionEntry } from "./ReviewSessionEntry";
 
 @ObjectType()
-@InputType("ReviewSessionInput")
-export class ReviewSessionInput {
-   @Field(() => Int)
-   user_id: number;
-
+@InputType("ReviewSessionWithoutUserIdInput")
+export class ReviewSessionWithoutUserId {
    @Field(() => Float)
    start_date: number;
 
@@ -28,8 +26,24 @@ export class ReviewSessionInput {
    saturation_threshold?: number;
 }
 
+@ObjectType()
+@InputType("ReviewSessionInput")
+export class ReviewSessionInput extends ReviewSessionWithoutUserId {
+   @Field(() => Int)
+   user_id: number;
+}
+
 @ObjectType("ReviewSession")
 export class ReviewSession extends ReviewSessionInput {
    @Field()
    review_session_id: number;
+}
+
+@ObjectType()
+export class SessionAndEntries {
+   @Field(() => ReviewSession)
+   session: ReviewSession;
+
+   @Field(() => [ReviewSessionEntry])
+   entries: ReviewSessionEntry[];
 }
